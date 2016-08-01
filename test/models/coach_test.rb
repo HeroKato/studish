70bda70158d0b20464aa0b0f3a07f72e71ae9bc5
herokato <h_htb_atm_k@yahoo.co.jp>
2016-08-01 14:22:34 +0000

    finished

--- test/models/coach_test.rb
+++ test/models/coach_test.rb
@@ -9,12 +9,7 @@ class CoachTest < ActiveSupport::TestCase
     assert_equal "football,football,football", coach.subject
     assert_equal "3年", coach.school_year
     assert_equal "Hi, I'm Messi. I'm a professional football player. I'm playing in FC Barcelona. I was very short when I was young.", coach.self_introduction
-    assert_equal Rack::Test::UploadedFile.new("app/assets/images/logo.png", "image/png"), coach.picture
+    assert_equal "#{Rails.root}/public/uploads/coach/picture/1/logo.png", coach.picture.file.file
   end
-  
-  test "authenticate" do
-    coach = FactoryGirl.create(:coach, name: "messi", picture: "coach1.jpg", password: "barcelona", password_confirmation: "barcelona")
-    assert_nil Coach.authenticate("messi", "real")
-    assert_equal coach, Coach.authenticate("messi", "barcelona")
-  end
+
 end
