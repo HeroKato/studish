4f4f8dd6b907a40c22c33887fc6a5a5050d7a5ae
herokato <h_htb_atm_k@yahoo.co.jp>
2016-08-11 05:44:58 +0000

    modified views/coaches/index and views/coaches/show

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @inquiry = Inquiry.new
     coach = Coach.where(activated: true)
-    @coach = coach.find( coach.pluck(:id).sample(7) ).sample(7)
+    @slide_coaches = coach.find( coach.pluck(:id).sample(7) ).sample(7)
   end
 end
