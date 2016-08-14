a3d60e5da7de70f6b2ce2cf8ec8cea281cfd950b
herokato <h_htb_atm_k@yahoo.co.jp>
2016-08-14 15:18:17 +0000

    added seeds/production/coaches.rb

---
+++ db/seeds/production/coaches.rb
@@ -0,0 +1,27 @@
+path = Rails.root.join("db/seeds/production/coach1.jpg")
+coach = Coach.create!(
+  name: "admin",
+  full_name: "admin",
+  email: "admin@studish.com",
+  birthday: "2016-7-9",
+  university: "バカ田大学",
+  major: "アホ学部マヌケ学科",
+  school_year: "1年",
+  self_introduction: "Hi, I'm a idiot. Killing it!",
+  administrator: "true",
+  password: "adminpass",
+  password_confirmation: "adminpass",
+  activated: true,
+  activated_at: Time.zone.now,
+  picture: open("#{path}"),
+  skype: "adminskype",
+  phone: "123-1234-5678"
+)
+CoachingSubject.create!(
+  coach: coach,
+  jr_english: '英語',
+  jr_japanese: '国語',
+  jr_math: '',
+  jr_science: '',
+  jr_social: ''
+)
