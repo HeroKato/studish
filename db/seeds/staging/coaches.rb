85586089a7a80e2362629e899cff605ee9ab1582
herokato <h_htb_atm_k@yahoo.co.jp>
2016-08-14 14:43:43 +0000

    did rake assets:precompile

--- db/seeds/staging/coaches.rb
+++ db/seeds/staging/coaches.rb
@@ -1,4 +1,4 @@
-Coach.create!(
+coach = Coach.create!(
   name: "admin",
   full_name: "admin",
   email: "admin@studish.com",
@@ -6,7 +6,6 @@ Coach.create!(
   university: "バカ田大学",
   major: "アホ学部マヌケ学科",
   school_year: "1年",
-  subject: "英語",
   self_introduction: "Hi, I'm a idiot. Killing it!",
   administrator: "true",
   password: "adminpass",
@@ -16,4 +15,12 @@ Coach.create!(
   picture: "app/assets/images/logo.png",
   skype: "adminskype",
   phone: "123-1234-5678"
+)
+CoachingSubject.create!(
+  coach: coach,
+  jr_english: '英語',
+  jr_japanese: '国語',
+  jr_math: '',
+  jr_science: '',
+  jr_social: ''
 )
