32e42ce72cd12cec27aa87644d231acc903d9707
herokato <h_htb_atm_k@yahoo.co.jp>
2016-08-17 11:45:40 +0000

    modified coaches/show and coaches/index, removed welcome/slide

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   def index
     @inquiry = Inquiry.new
     coach = Coach.where(activated: true)
-    @slide_coaches = coach.find( coach.pluck(:id).sample(7) ).sample(7)
+    @coaches = coach.find( coach.pluck(:id).sample(6) ).sample(6)
   end
 end
