baa69030873871060c9cc1525acf2661b85a6c87
herokato <h_htb_atm_k@yahoo.co.jp>
2016-08-22 18:10:28 +0000

    modified seeds/develop/coching_reports.rb end comments/_index.html.erb

--- db/seeds/development/coaching_reports.rb
+++ db/seeds/development/coaching_reports.rb
@@ -1,4 +1,4 @@
-report =
+body =
   "This is a test report.\n" +
   "I'm listening to Andrew Rayel" +
   "and eating cookies." +
@@ -10,28 +10,24 @@ report =
   "でもまだ仕事中で観れないので、予選は我慢して準決勝あたりから観ようと思う。"+
   "決勝は早朝なので観ないかもしれない。"+
   "最後はめんどくさかったので日本語で書いちゃいました。"
-comment =
-  "This is a test comment.\n"
 %w(example1 example2 example3).each do |name|
   coach = Coach.find_by(name: name)
   0.upto(100) do |idx|
     report = CoachingReport.create(
       author: coach,
       title: "coaching_report#{idx}",
-      body: report,
-      posted_at: 10.days.ago.advance(days: idx),
-      status: %w(draft public_for_coaches unpublic_for_coaches)[idx % 3]
-    )
+      body: body,
+      posted_at: 100.days.ago.advance(days: idx),
+      status: %w(draft public_for_coaches unpublic_for_coaches)[idx % 3])
+    report_id = report.id
     if idx == 7 || idx == 8
       %w(example4 example5 example6).each do |name2|
         commenter = Coach.find_by(name: name2)
-        commenter.commented_reports << report
-        0.upto(3) do |idx|
-          Comment.create(
-            coach: commenter,
-            coaching_report: report,
-            body: comment)
-        end
+        Comment.create(
+          coach: commenter,
+          coaching_report_id: report_id,
+          commenter: name2,
+          body: 'this is a test comment.')
       end
     end
   end
