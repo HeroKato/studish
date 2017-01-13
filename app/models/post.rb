25abbd5b0185067ec02fef90a877afcfa056ff27
herokato <h_htb_atm_k@yahoo.co.jp>
2017-01-13 12:51:32 +0000

    added feature

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,9 +10,13 @@ class Post < ActiveRecord::Base
   validates :student_id, presence: true
   validates :caption, presence: true, length: { maximum: 1000 }
   validates :subject, presence: true, length: { maximum: 20 }
-  validates :text_name, presence: true, length: { maximum: 100 }
-  validates :number, presence: true, length: { maximum: 30 }
+  validates :text_name, presence: true, unless: :question_type_how_to_study?, length: { maximum: 100 }
+  validates :number, presence: true, unless: :question_type_how_to_study?, length: { maximum: 30 }
   validates :chapter, length: { maximum: 50 }
   validates :section, length: { maximum: 50 }
   validates :pattern, length: { maximum: 50 }
+  
+  def question_type_how_to_study?
+    subject == "勉強方法"
+  end
 end
